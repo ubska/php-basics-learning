d154bbe6ccec524224462791651c41f6d255f4e8
ubska <alessandraubska@gmail.com>
2025-04-24 16:05:12 +0200

    es. 5

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
+// Esercizio 4: Calcolare l'area di un rettangolo
+// Obiettivo: Dato un rettangolo, calcolare e stampare l'area (area = base * altezza).
 
-// Esercizio 1: Crea una variabile $città e assegna il nome di una città. Stampa un messaggio che dice "Vivo a [nome della città]".
+$base = 5;
+$altezza = 3;
+$area = $base * $altezza;
 
-// Esercizio 2: Crea una variabile $anno_nascita e calcola l'età di una persona (supponendo che l'anno corrente sia 2025).
-
-// Esercizio 3: Concatenare il tuo nome e cognome in una singola variabile e stamparla.
-
-$nome = "Mario";
-$cognome = "Rossi";
-$nome_intero = $nome  . $cognome;
-
-echo $nome_intero;
+echo "L'area del rettangolo è: "  . $area . " metri quadri";
