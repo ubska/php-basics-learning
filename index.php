d49126cfe48c74d6e2df152921ef743684db6875
ubska <alessandraubska@gmail.com>
2025-04-24 16:10:52 +0200

    es.6

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-// Esercizio 4: Calcolare l'area di un rettangolo
-// Obiettivo: Dato un rettangolo, calcolare e stampare l'area (area = base * altezza).
+// Esercizio 6: Calcolare lo sconto su un prodotto
+// Obiettivo: Supponiamo di avere un prodotto che costa 100€ e di applicare uno sconto del 20%. Calcolare il prezzo finale dopo lo sconto.
 
-$base = 5;
-$altezza = 3;
-$area = $base * $altezza;
+$prezzo_iniziale = 100;
+$sconto = 20;
+$importo_sconto = ($prezzo_iniziale * $sconto) / 100;
+$prezzo_finale = $prezzo_iniziale - $importo_sconto;
 
-echo "L'area del rettangolo è: "  . $area . " metri quadri";
+echo "Il prezzo finale dopo lo sconto è: " . $prezzo_finale . "£";
