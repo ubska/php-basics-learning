09c39a347ba5b21a6ba56b43adc3cf9cd01df305
ubska <alessandraubska@gmail.com>
2025-04-24 15:17:22 +0200

    concatenazione di stringhe con il .

--- index.php
+++ index.php
@@ -3,3 +3,6 @@ $name = "Mario"; //stringa
 $age = 25; //intero
 $price = 19.99; //float
 $isActive = true; // booleano
+
+// La concatenazione di stringhe in PHP viene fatta con l'operatore .
+echo "Ciao " . $name; // ciao Mario
